2126a79b0b0ce0a23281adc1029f5a8b80728773
Jordan Justen <jljusten@gmail.com>
2009-10-20 21:54:23 -0700

    Add build.sh shell script.

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ -z "$EDK_TOOLS_PATH" ]
+  export EDK_TOOLS_PATH=`pwd`/BaseTools
+fi
+if [ -z "$WORKSPACE" ]
+then
+  . BaseTools/BuildEnv > /dev/null
+else
+  . $WORKSPACE/BaseTools/BuildEnv > /dev/null
+fi
+if [ $? -ne 0 ]
+then
+  echo "Failed to setup build environment"
+  exit -1
+fi
+
+build $*
+
